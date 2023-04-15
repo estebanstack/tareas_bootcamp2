5b9809ab00771177446ed94aebe5bad51a59ae3d
estebanstack <130319203+estebanstack@users.noreply.github.com>
2023-04-14 20:39:02 -0500

    Add files via upload

--- doe.java
+++ doe.java
@@ -1,13 +1,12 @@
 public class doe {
     public static void main(String[] args) {
-        int contador = 2;
-        do { 
+        int contador = 4;
+        do {
+            
             System.out.println(contador);
-            contador++;
+            contador++; 
             
         } while (contador < 3);
-        System.out.println(contador);
-        contador++;
         
     }
     
